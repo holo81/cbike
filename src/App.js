9e0a27aa724b3c9b8289f994f70f3db60863854f
Antti Holopainen <antti.holopainen@f-secure.com>
2017-04-10 17:03:51 +0300

    new stuff

--- src/App.js
+++ src/App.js
@@ -2,20 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  ApolloClient,
+  gql,
+  graphql,
+  ApolloProvider,
+  createNetworkInterface
+} from 'react-apollo';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface(
+    { uri: 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql' })
+});
+
+const ChannelsList = ({ data: { loading, error, bikeRentalStations } }) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  return <ul>
+    {bikeRentalStations.map(ch => <li key={ch.stationId}>{ch.name}</li>)}
+  </ul>;
+};
+
+const channelsListQuery = gql`query MyQuery { bikeRentalStations {
+    stationId
+    name
+    lat
+    lon
+    bikesAvailable
+    spacesAvailable
+  }
+}
+ `;
+
+const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Apollo</h2>
+          </div>
+          <ChannelsListWithData />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </ApolloProvider>
     );
   }
 }
-
 export default App;
