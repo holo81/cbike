178226c6c4304b14768b13f4865938a54b154bcb
Antti Holopainen <antti.holopainen@f-secure.com>
2017-04-12 16:33:16 +0300

    Geolocation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
   createNetworkInterface
 } from 'react-apollo';
 
+import { geolocated } from 'react-geolocated';
+
 const client = new ApolloClient({
   networkInterface: createNetworkInterface(
     { uri: 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql' })
@@ -22,14 +24,13 @@ const ChannelsList = ({ data: { loading, error, nearest } }) => {
   if (error) {
     return <p>{error.message}</p>;
   }
-  console.log(nearest.edges)
   return <table>
     <thead><tr><th>Place</th><th>Distance</th><th>Bikes Available</th><th>Places empty</th></tr></thead>
     <tbody>
-      {nearest.edges.map(ch => <tr id={ch.node.place.stationId}>
+      {nearest.edges.map(ch => <tr key={ch.node.place.stationId}>
         <td>
           <a href={`https://maps.google.com?saddr=Current+Location&daddr=${ch.node.place.lat},${ch.node.place.lon}`}>{ch.node.place.name}</a>
-          </td>
+        </td>
         <td>{ch.node.distance} m</td>
         <td>{ch.node.place.bikesAvailable}</td>
         <td>{ch.node.place.spacesAvailable}</td>
@@ -85,6 +86,33 @@ const NewChannelsListWithData = graphql(newQuery, {
   })
 })(ChannelsList);
 
+class Geoloc extends React.Component {
+  render() {
+    return !this.props.isGeolocationAvailable
+      ? <div>Your browser does not support Geolocation</div>
+      : !this.props.isGeolocationEnabled
+        ? <div>Geolocation is not enabled</div>
+        : this.props.coords
+          ? <table>
+            <tbody>
+              <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
+              <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
+              <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
+              <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
+              <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
+            </tbody>
+          </table>
+          : <div>Getting the location data&hellip; </div>;
+  }
+}
+
+const WrapGeolocated = geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000
+})(Geoloc);
+
 class App extends Component {
   render() {
     return (
@@ -93,6 +121,7 @@ class App extends Component {
           <div className="App-header">
             <h2>HSL Citybikes</h2>
           </div>
+          <WrapGeolocated />
           <NewChannelsListWithData />
         </div>
       </ApolloProvider>
