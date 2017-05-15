cb8d6c544784d755397187a8c612e23e9d55aa49
Holopainen <holoan@F-Secure.com>
2017-05-15 15:28:57 +0300

    trying new stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { withState, pure, compose } from 'recompose';
 import './App.css';
 
 import {
@@ -10,122 +10,116 @@ import {
   createNetworkInterface
 } from 'react-apollo';
 
-import { geolocated } from 'react-geolocated';
-
 const client = new ApolloClient({
   networkInterface: createNetworkInterface(
-    { uri: 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql' })
+  { uri: 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql' })
 });
 
-const ChannelsList = ({ data: { loading, error, nearest } }) => {
-  if (loading) {
-    return <p>Loading ...</p>;
-  }
-  if (error) {
-    return <p>{error.message}</p>;
-  }
-  return <table>
-    <thead><tr><th>Place</th><th>Distance</th><th>Bikes Available</th><th>Places empty</th></tr></thead>
-    <tbody>
-      {nearest.edges.map(ch => <tr key={ch.node.place.stationId}>
-        <td>
-          <a href={`https://maps.google.com?saddr=Current+Location&daddr=${ch.node.place.lat},${ch.node.place.lon}`}>{ch.node.place.name}</a>
-        </td>
-        <td>{ch.node.distance} m</td>
-        <td>{ch.node.place.bikesAvailable}</td>
-        <td>{ch.node.place.spacesAvailable}</td>
+class StationListPure extends Component {
+    constructor(props) {
+      super(props);
+    }
+    
+    
+    render() {
+      
+      if (this.props.data.loading) {
+        return <p>Loading ...</p>;
+      }
+      if (this.props.data.error) {
+        return <p>{this.props.data.error.message}</p>;
+      }
+      console.log(this.props.data.nearest)
+      return <table>
+      <thead><tr><th>Place</th><th>Distance</th><th>Bikes Available</th><th>Places empty</th></tr></thead>
+      <tbody>
+      {this.props.data.nearest.edges.map(ch => <tr key={ch.node.place.stationId}>
+      <td>
+      <a href={`https://maps.google.com?&dirflg=b&saddr=Current+Location&daddr=${ch.node.place.lat},${ch.node.place.lon}`}>{ch.node.place.name}</a>
+      </td>
+      <td>{ch.node.distance} m</td>
+      <td>{ch.node.place.bikesAvailable}</td>
+      <td>{ch.node.place.spacesAvailable}</td>
       </tr>)}
-    </tbody>
-  </table>;
-};
-
-const channelsListQuery = gql`query MyQuery { bikeRentalStations {
-    stationId
-    name
-    lat
-    lon
-    bikesAvailable
-    spacesAvailable
+      </tbody>
+      </table>;
+    }
   }
-}
- `;
 
-const newQuery = gql`
- query foo($lat:Float, $lon:Float, $maxDistance:Int, $maxResults:Int) {nearest(lat:$lat, lon:$lon, maxDistance:$maxDistance, maxResults:$maxResults, filterByPlaceTypes:[BICYCLE_RENT])
+const queryData = graphgql(`
+query foo($lat:Float, $lon:Float, $maxDistance:Int, $maxResults:Int) {nearest(lat:$lat, lon:$lon, maxDistance:$maxDistance, maxResults:$maxResults, filterByPlaceTypes:[BICYCLE_RENT])
   {edges
-          {node
-                {id
-                  distance
+    {node
+      {id
+        distance
         place {
           __typename
           ... on BikeRentalStation{id
-          stationId
-          name
-          bikesAvailable
-          spacesAvailable
-          lon
-          lat
+            stationId
+            name
+            bikesAvailable
+            spacesAvailable
+            lon
+            lat
           }
         }
       }
     }
   }
 }
-`;
+`, {
+    options: (ownProps) => ({
+      variables: {
+        lat: ownProps.state.lat,
+        lon: ownProps.state.lon,
+        maxDistance: 10000,
+        maxResults: 500
+      }
+    }),
+});
 
-const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
-const NewChannelsListWithData = graphql(newQuery, {
-  options: (ownProps) => ({
-    // pollInterval: 5000,
-    variables: {
-      lat: 60.1756332,
-      lon: 24.9150216,
-      maxDistance: 10000,
-      maxResults: 500
+const StationSearchResults = compose(
+  data,
+  pure,
+)(StationListPure);
+
+const location = withState('location', {lat:0, lon:0})
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition((pos) => {
+    var crd = pos.coords;
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
     }
-  })
-})(ChannelsList);
-
-class Geoloc extends React.Component {
-  render() {
-    return !this.props.isGeolocationAvailable
-      ? <div>Your browser does not support Geolocation</div>
-      : !this.props.isGeolocationEnabled
-        ? <div>Geolocation is not enabled</div>
-        : this.props.coords
-          ? <table>
-            <tbody>
-              <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
-              <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
-              <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
-              <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
-              <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
-            </tbody>
-          </table>
-          : <div>Getting the location data&hellip; </div>;
+    , (err) => {console.warn(`ERROR(${err.code}): ${err.message}`);}
+    , {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    });
   }
-}
-
-const WrapGeolocated = geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000
-})(Geoloc);
-
-class App extends Component {
-  render() {
-    return (
+  
+  
+  
+  
+  
+  const NewChannelsListWithData = graphql(newQuery
+  })(StationList);
+  
+  class App extends Component {
+    render() {
+      return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <div className="App-header">
-            <h2>HSL Citybikes</h2>
-          </div>
-          <WrapGeolocated />
-          <NewChannelsListWithData />
-        </div>
+      <div className="App">
+      <div className="App-header">
+      <h2>HSL Citybikes</h2>
+      </div>
+      <NewChannelsListWithData />
+      </div>
       </ApolloProvider>
-    );
+      );
+    }
   }
-}
-export default App;
+  export default App;
